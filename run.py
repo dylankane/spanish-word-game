10072637642625c21d3ba1a26d53f732c6e6112c
Dylan <dylankane3@gmail.com>
2023-03-08 15:22:29 +0000

    Add function to ask user to begin game

--- run.py
+++ run.py
@@ -1,15 +1,36 @@
 import random
 from words import WORDS
 
-# word = random.choice(WORDS)
-# print(word)
 
+def get_word():
+    word = random.choice(WORDS)
+    print(word)
+
+
+def start_game():
+    '''
+    Function to ask user if they are ready to start the game
+    '''
+    print("Type Y for yes to the start game")
+    inp = input().lower()
+    if inp == "y":
+        get_word()
+    else:
+        print("You must type 'y' or 'n' ")
+
+
+print("")
 print("****SPANISH WORD GAME****")
 print("")
 print("-----------The Rules-----------")
 print("-Start your game, and you will be given a word-")
-print("-All you have to do is type the english translation to this word-")
-print("-You have 3 lives and to make 20 correct answers to win the game-")
+print("-Just type the english translation for this word-")
+print("-You have 3 lives to make 20 correct answers to win the game-")
+print("")
+print("")
 
+start_game()
 
-# start_game()
+# word = random.choice(WORDS)
+# print(word)
+# python3 run.py
