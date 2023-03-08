6f1a77dd8b84f58a870bde6decbf65b870e78145
Dylan <dylankane3@gmail.com>
2023-03-08 10:29:49 +0000

    Add file to hold word list, and set up basic list

---
+++ words.py
@@ -0,0 +1,18 @@
+WORDS = [
+    {
+        "english": "cat",
+        "spanish": "gato"
+    },
+    {
+        "english": "dog",
+        "spanish": "perro"
+    },
+    {
+        "english": "cow",
+        "spanish": "vaca"
+    },
+    {
+        "english": "frog",
+        "spanish": "rana"
+    }
+]
