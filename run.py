a4d0782a2b0b27ba0c3033953f3384148901d5a1
Dylan <dylankane3@gmail.com>
2023-03-08 12:32:26 +0000

    Add title and game rules to print to terminal

--- run.py
+++ run.py
@@ -1,7 +1,15 @@
 import random
 from words import WORDS
 
-word = random.choice(WORDS)
-print(word)
+# word = random.choice(WORDS)
+# print(word)
+
+print("****SPANISH WORD GAME****")
+print("")
+print("-----------The Rules-----------")
+print("-Start your game, and you will be given a word-")
+print("-All you have to do is type the english translation to this word-")
+print("-You have 3 lives and to make 20 correct answers to win the game-")
 
 
+# start_game()
