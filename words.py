8fd8440a8f0a8696aa052d4b2ededd640b555264
Dylan <dylankane3@gmail.com>
2023-03-11 22:51:57 +0000

    Create larger list of words in words.py

--- words.py
+++ words.py
@@ -14,5 +14,189 @@ WORDS = [
     {
         "english": "frog",
         "spanish": "rana"
+    },
+    {
+        "english": "year",
+        "spanish": "año"
+    },
+    {
+        "english": "clock",
+        "spanish": "reloj"
+    },
+    {
+        "english": "hello",
+        "spanish": "hola"
+    },
+    {
+        "english": "goodbye",
+        "spanish": "adios"
+    },
+    {
+        "english": "bad",
+        "spanish": "malo"
+    },
+    {
+        "english": "one",
+        "spanish": "uno"
+    },
+    {
+        "english": "sun",
+        "spanish": "sol"
+    },
+    {
+        "english": "hand",
+        "spanish": "mano"
+    },
+    {
+        "english": "hair",
+        "spanish": "pelo"
+    },
+    {
+        "english": "grape",
+        "spanish": "uva"
+    },
+    {
+        "english": "boot",
+        "spanish": "bota"
+    },
+    {
+        "english": "table",
+        "spanish": "mesa"
+    },
+    {
+        "english": "sea",
+        "spanish": "mar"
+    },
+    {
+        "english": "bag",
+        "spanish": "bolsa"
+    },
+    {
+        "english": "bed",
+        "spanish": "cama"
+    },
+    {
+        "english": "girl",
+        "spanish": "niña"
+    },
+    {
+        "english": "love",
+        "spanish": "amor"
+    },
+    {
+        "english": "here",
+        "spanish": "aquí"
+    },
+    {
+        "english": "ship",
+        "spanish": "barco"
+    },
+    {
+        "english": "baby",
+        "spanish": "bebé"
+    },
+    {
+        "english": "pen",
+        "spanish": "boli"
+    },
+    {
+        "english": "house",
+        "spanish": "casa"
+    },
+    {
+        "english": "car",
+        "spanish": "coche"
+    },
+    {
+        "english": "ten",
+        "spanish": "diez"
+    },
+    {
+        "english": "island",
+        "spanish": "isla"
+    },
+    {
+        "english": "wolf",
+        "spanish": "lobo"
+    },
+    {
+        "english": "moon",
+        "spanish": "luna"
+    },
+    {
+        "english": "frog",
+        "spanish": "rana"
+    },
+    {
+        "english": "lemon",
+        "spanish": "limon"
+    },
+    {
+        "english": "strawberry",
+        "spanish": "fresa"
+    },
+    {
+        "english": "duck",
+        "spanish": "pato"
+    },
+    {
+        "english": "monday",
+        "spanish": "lunes"
+    },
+    {
+        "english": "monkey",
+        "spanish": "mono"
+    },
+    {
+        "english": "pear",
+        "spanish": "pera"
+    },
+    {
+        "english": "chicken",
+        "spanish": "pollo"
+    },
+    {
+        "english": "kitchen",
+        "spanish": "cocina"
+    },
+    {
+        "english": "door",
+        "spanish": "puerta"
+    },
+    {
+        "english": "rain",
+        "spanish": "lluvia"
+    },
+    {
+        "english": "river",
+        "spanish": "río"
+    },
+    {
+        "english": "broken",
+        "spanish": "roto"
+    },
+    {
+        "english": "red",
+        "spanish": "rojo"
+    },
+    {
+        "english": "green",
+        "spanish": "verde"
+    },
+    {
+        "english": "pink",
+        "spanish": "rosa"
+    },
+    {
+        "english": "white",
+        "spanish": "blanco"
+    },
+    {
+        "english": "water",
+        "spanish": "agua"
+    },
+    {
+        "english": "bread",
+        "spanish": "pan"
     }
 ]
